46f26337b6be91e4bfe0231d62e587df176d64c4
gfleury <gfleury@ip-172-31-13-97.us-west-2.compute.internal>
2017-04-09 01:10:39 +0000

    README

--- overlayroot/mount-overlayroot.sh
+++ overlayroot/mount-overlayroot.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
+#  Copyright, 2012 Dustin Kirkland <kirkland@ubuntu.com>
+#  Copyright, 2012 Scott Moser <smoser@ubuntu.com>
+#  Copyright, 2012 Axel Heider
+#             2016 George Fleury <gfleury@gmail.com>
+#
+#  Based on scripts from
+#    Sebastian P.
+#    Nicholas A. Schembri State College PA USA
+#    Axel Heider
+#    Dustin Kirkland
+#    Scott Moser
+#
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see
+#    <http://www.gnu.org/licenses/>.
 
 . /lib/dracut-lib.sh
 
